d2fe3631324d3b16ea28277a777bace46f5e03e4
Anthony Grimes <i@raynes.me>
2012-04-29 23:14:34 -0500

    Fix description.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject lein-gitify "0.1.0"
-  :description "A leiningen plugin for gitifying a plugin."
+  :description "A leiningen plugin for gitifying a project."
   :url "https://github.com/Raynes/lein-gitify"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
